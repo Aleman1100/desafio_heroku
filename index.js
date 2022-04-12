665639fd809298f13249606554d75ceda778d593
Alejandro Bacquet <89495225+Aleman1100@users.noreply.github.com>
2022-04-12 19:32:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.listen(port, console.log(`Up en ${port}`));
