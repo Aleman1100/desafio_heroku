2392e3ce1d1d56d538079d2867951c8154ab6ac6
Alejandro Bacquet <89495225+Aleman1100@users.noreply.github.com>
2022-04-12 20:31:47 -0400

    Update consultas.js

--- consultas.js
+++ consultas.js
@@ -1,10 +1,9 @@
 const { Pool } = require('pg');
 const pool = new Pool ({
-    user: 'tmalvhhdsbupgm',
-    host: 'ec2-18-215-96-22.compute-1.amazonaws.com',
-    password: '59d83d268d42212845abcb1752e6cb49583578a5f7d59cab69a6a1e02a0846ab',
-    database: 'dan2kou3pgom45',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+        ssl: {
+        rejectUnauthorized: false
+        }
 });
 
 async function nuevoUsuario(username, email, password) {
